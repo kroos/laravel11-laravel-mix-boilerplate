c4945dbb20c09e7dc1d299c21568a8bf3b7e9334
Kroos <dhiauddin@gmail.com>
2025-01-20 16:38:20 +0800

    -- fix webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,15 +13,15 @@ mix.webpackConfig({
 });
 
 mix.js('resources/js/app.js', 'public/js/app.js')
-		.sass('resources/scss/app.scss', 'public/css/app.css')
+//		.sass('resources/scss/app.scss', 'public/css/app.css')
 		.postCss('resources/css/app.css', 'public/css/app.css', [
 				require('postcss-custom-properties'),
 				require('tailwindcss'),
 		])
-		.combine([
-		'public/css/app.css',
-		'./node_modules/select2-theme-bootstrap5/dist/select2-bootstrap.css',
-		], 'public/css/app.css')
+	//	.combine([
+	//	'public/css/app.css',
+	//	'./node_modules/select2-theme-bootstrap5/dist/select2-bootstrap.css',
+	//	], 'public/css/app.css')
 	// .setPublicPath('public/)
 	.sourceMaps()
 ;
