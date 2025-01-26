771a9ca5e1003c32a5f28287a141efc57e5017cc
kroos <dhiauddin@gmail.com>
2025-01-27 04:41:55 +0800

    -- fix on webpack

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,13 +46,13 @@ $currentYear = Carbon::now()->year;
 	</div>
 
 </body>
-<script src="{{ asset('js/fullcalendar/index.global.js') }}"></script>
-<script src="{{ asset('js/chart.js/chart.umd.js') }}"></script>
-<!-- <script src="https://unpkg.com/popper.js/dist/umd/popper.min.js"></script> -->
-<!-- <script src="https://unpkg.com/tooltip.js/dist/umd/tooltip.min.js"></script> -->
-<script src="{{ asset('js/app.js') }}"></script>
+<script src="{{ mix('js/fullcalendar/index.global.js') }}"></script>
+<script src="{{ mix('js/chart.js/dist/chart.umd.js') }}"></script>
+<script src="{{ mix('js/app.js') }}"></script>
 <script src="{{ asset('js/ckeditor/ckeditor.js') }}"></script>
 <script src="{{ asset('js/ckeditor/adapters/jquery.js') }}"></script>
+<!-- <script src="https://unpkg.com/popper.js/dist/umd/popper.min.js"></script> -->
+<!-- <script src="https://unpkg.com/tooltip.js/dist/umd/tooltip.min.js"></script> -->
 <script >
 	jQuery.noConflict ();
 	(function($){
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,6 @@
 // const mix = require('laravel-mix');
 let mix = require('laravel-mix');
-const path = require('path')
+const path = require('path');
 
 mix.webpackConfig({
 //	plugins: [
@@ -13,22 +13,27 @@ mix.webpackConfig({
 });
 
 mix.js('resources/js/app.js', 'public/js/app.js')
-//		.sass('resources/scss/app.scss', 'public/css/app.css')
-		.postCss('resources/css/app.css', 'public/css/app.css', [
-			require('postcss-custom-properties'),
-			require('tailwindcss'),
-		])
-	//	.scripts([
-	//		'node_modules/chartjs/chart.js',
-	//		'node_modules/fullcalendar/index.global.js'
-	//	], 'public/js/app.js')
-	//	.styles([
-	//		'node_modules/chartjs/chart.css',
-	//		'node_modules/fullcalendar/index.global.css'
-	//	], 'public/js/app.css')
-	//	.combine([
-	//	'public/css/app.css',
-	//	'./node_modules/select2-theme-bootstrap5/dist/select2-bootstrap.css',
-	//	], 'public/css/app.css')
+	.postCss('resources/css/app.css', 'public/css/app.css', [
+		require('postcss-custom-properties'),
+		require('tailwindcss'),
+	])
+	// .sass('resources/scss/app.scss', 'public/css/app.css')
+	// .scripts([
+	// 	'node_modules/chartjs/chart.js',
+	// 	'node_modules/fullcalendar/index.global.js'
+	// ], 'public/js/app.js')
+	// .styles([
+	// 	'node_modules/chartjs/chart.css',
+	// 	'node_modules/fullcalendar/index.global.css'
+	// ], 'public/js/app.css')
+	// .combine([
+	// 	'public/css/app.css',
+	// 	'./node_modules/select2-theme-bootstrap5/dist/select2-bootstrap.css',
+	// ], 'public/css/app.css')
 	// .setPublicPath('public/)
+	// .autoload({
+	// 	jquery: ['$', 'window.jQuery']
+	// })
+	.copyDirectory('node_modules/fullcalendar', 'public/js/fullcalendar')
+	.copyDirectory('node_modules/chart.js', 'public/js/chart.js')
 	.sourceMaps();
